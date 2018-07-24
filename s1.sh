c1c702a14e72601564d6323e26d68941b267fec2
ANtonio <tonodx17@ciencias.unam.mx>
2018-07-24 13:21:11 -0500

    scripts prope

---
+++ s1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "inicia el script 1"
+ls
+mkdir ./magiaa
+touch ./magiaa/alibaba
+echo "Hocus pocus" > ./magiaa/alibaba
+cat ./magiaa/alibaba
+echo "Fin del script 1"
+
---
+++ s2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "inicia el script 1"
